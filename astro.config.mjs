c8ade3a09f3f403cd9a931aa6b98475ce5d53e57
Rafhael Marsigli <rmarsigli@gmail.com>
2026-03-03 18:22:59 -0300

    chore(seo): integrate astro js sitemap and set absolute production url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dutra-farma.sites.rushcms.com',
   vite: {
     plugins: [
       tailwindcss(),
       Icons({ compiler: 'astro' })
     ]
-  }
+  },
+
+  integrations: [sitemap()]
 });
