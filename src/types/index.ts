840a4165ec1f4d5153795fa55e03636b2ed498be
Rafhael Marsigli <rmarsigli@gmail.com>
2026-03-03 16:38:59 -0300

    feat: content updates and new units format

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,12 @@
 export interface Unit {
-  id: string
-  name: string
-  address: string
-  neighborhood: string
-  city: string
-  state: string
-  phone: string
-  whatsapp?: string
-  coordinates: [number, number]
-  maps_url?: string
+	id: string
+	name: string
+	address: string
+	neighborhood: string
+	city: string
+	state: string
+	phone: string
+	whatsapp?: string
+	coordinates: [number, number]
+	maps_url?: string
 }
